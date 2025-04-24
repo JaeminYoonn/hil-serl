4c208d856b80c32a64ad0e1003814b8b4cdf95da
jaemin.yoon <jaemin.yoon@holiday-robotics.com>
2025-04-24 15:23:42 +0900

    add holly_pickup_insertion exp

---
+++ examples/experiments/holly_pickup_insertion/run_learner.sh
@@ -0,0 +1,7 @@
+export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
+python ../../train_rlpd.py "$@" \
+    --exp_name=holly_pickup_insertion \
+    --checkpoint_path=../../experiments/holly_pickup_insertion/debug \
+    --demo_path=... \
+    --learner \
